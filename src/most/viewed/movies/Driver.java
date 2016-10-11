8b01c3bdd5f2642dcc121bb35748dcafa3dcac60
Abhishek Verma <www.abhiverma23@gmail.com>
2016-10-11 15:55:56 +0530

    Most Viewed Movies: Driver Class Updated and Working

--- src/most/viewed/movies/Driver.java
+++ src/most/viewed/movies/Driver.java
@@ -8,9 +8,12 @@ package most.viewed.movies;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.io.Text;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 /**
@@ -46,8 +49,10 @@ public class Driver {
 		job.setReducerClass(Red.class);
 		job.setInputFormatClass(TextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 		System.exit(job.waitForCompletion(false) ? 0 : 1);
 	}
