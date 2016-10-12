75f9cea058bf55aa850e63bc1ca9a6ac22c49d8c
Abhishek Verma <www.abhiverma23@gmail.com>
2016-10-12 19:02:21 +0530

    Filter Users: Valid user given 40 Plus ratings [SUCCESS]

---
+++ src/filter/users/Com.java
@@ -0,0 +1,31 @@
+/**
+ * @author Abhishek Verma
+ * @email abhishekverma3210@gmail.com
+ */
+package filter.users;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * @author Abhishek Verma
+ * @email abhishekverma3210@gmail.com
+ */
+public class Com extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+	/**
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	@Override
+	protected void reduce(IntWritable arg0, Iterable<IntWritable> arg1,
+			Context arg2) throws IOException, InterruptedException {
+		int sum = 0;
+		for(IntWritable v : arg1){
+			sum+=v.get();
+		}
+		if(sum>=40)
+			arg2.write(arg0, new IntWritable(sum));
+	}
+}
---
+++ src/filter/users/Driver.java
@@ -0,0 +1,59 @@
+/**
+ * @author Abhishek Verma
+ * @email abhishekverma3210@gmail.com
+ * @version 0.0.0
+ */
+package filter.users;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+/**
+ * 
+ */
+public class Driver {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws InterruptedException
+	 */
+	public static void main(String[] args) throws IOException,
+			InterruptedException, ClassNotFoundException {
+
+		if (args.length != 2) {
+			System.out.println("USAGES : " +
+					"hadoop jar <JARNAME>.jar <DIRIVERCLASSNAME>" +
+					" <INPUTFILE> <OUTPUTDIRECTORY> {ENTER}");
+			System.exit(2);
+		}
+
+		System.out.println("---Getting User Given 40+ Ratings---");
+
+		Configuration conf = new Configuration();
+
+		Job job = new Job(conf, "User 40+ Ratings");
+
+		job.setJarByClass(Driver.class);
+		job.setMapperClass(Map.class);
+		job.setCombinerClass(Com.class);
+		job.setReducerClass(Red.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setOutputKeyClass(IntWritable.class);
+		job.setOutputValueClass(IntWritable.class);
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		System.exit(job.waitForCompletion(false) ? 0 : 1);
+	}
+}
