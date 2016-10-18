91230b679ef1afdf9b50d891999e572643dbf32a
Abhishek Verma <www.abhiverma23@gmail.com>
2016-10-18 11:02:25 +0530

    MoviesRating: Working [SUCCESS]

---
+++ src/movies/ratings/Com.java
@@ -0,0 +1,47 @@
+/**
+ * @author Abhishek Verma
+ * @email abhishekverma3210@gmail.com
+ */
+package movies.ratings;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+/**
+ * @author Abhishek Verma
+ * @email abhishekverma3210@gmail.com
+ */
+public class Com extends Reducer<IntWritable, Text, IntWritable, Text> {
+	/**
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	@Override
+	protected void reduce(IntWritable arg0, Iterable<Text> arg1,
+			Context arg2) throws IOException, InterruptedException {
+		System.out.println("Combiner Code is beeing Executed");
+		boolean isValidUser=false;
+		String str="";
+		for(Text t:arg1){
+			if(t.toString().equals("$$VALID$$")) {
+				isValidUser = true;
+			}
+			else{
+				str+=t.toString()+":";
+			}
+		}
+		String[] st = str.split(":");
+		String s[];
+		if(isValidUser){
+			for (String aSt : st) {
+				s = aSt.split(",");
+				if (s.length == 2) {
+					arg2.write(new IntWritable(Integer.parseInt(s[0])), new Text(s[1]));
+				}
+			}
+		}
+	}
+}
---
+++ src/movies/ratings/Map.java
@@ -0,0 +1,64 @@
+/**
+ * @author Abhishek Verma
+ * @email abhishekverma3210@gmail.com
+ */
+package movies.ratings;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+/**
+ * @author Abhishek Verma
+ * email abhishekverma3210@gmail.com
+ *
+ */
+public class Map extends Mapper<Object, Text, IntWritable, Text> {
+	
+	private IntWritable userId = new IntWritable();
+	
+	/**
+	 * @throws InterruptedException
+	 * @throws IOException
+	 */
+	@Override
+	protected void map(Object key, Text value, Context context)
+			throws IOException, InterruptedException {
+		// ratings.csv format userId,movieId,rating,timestamp
+		 	
+		FileSplit fileSplit = (FileSplit)context.getInputSplit();
+		String fileName = fileSplit.getPath().getName();
+		String str[] = value.toString().split(",");
+		if(fileName.equals("ratings.csv")){
+			if(str.length!=4){
+				System.out.println("Unwanted data in ratings.csv");
+			}
+			else{
+				try{
+					userId.set(Integer.parseInt(str[0]));
+					context.write(userId,
+							new Text(str[1]
+							+ "," + str[2]));
+				}catch (NumberFormatException e) {
+					System.out.println("Found Improper movieId => \"" + str[0] + "\"");
+				}
+			}
+		}
+		else{
+			if(str.length!=2){
+				System.out.println("Unwanted data in Valid Users list");
+			}
+			else{
+				try{
+					userId.set(Integer.parseInt(str[0]));
+					context.write(userId, new Text("$$VALID$$"));
+				}catch(NumberFormatException e){
+					System.out.println("Found Improper movieId => \"" + str[0] + "\"");
+				}
+			}
+		}
+	}
+}
