a7d802171d9f530673002c4f65156bc391f5f8da
Abhishek Verma <www.abhiverma23@gmail.com>
2016-10-19 13:15:21 +0530

    Top20 : Fetching 10 records on basses of value [SUCCESS]
    Challanges : Text is not supported in HashMap(confirmed) or TreeMap(not confirmed about it) for doing sorting with respect to KEYS. Optimizaion required.

--- src/top/twenty/records/Map.java
+++ src/top/twenty/records/Map.java
@@ -21,7 +21,7 @@ import java.util.Map.Entry;
  */
 public class Map extends Mapper<Object, Text, Text, NullWritable> {
 
-	private HashMap<Text, Double> top20 = new HashMap<Text, Double>();
+	private static HashMap<String, Double> top20 = new HashMap<String, Double>();
 	private final IntWritable one = new IntWritable(1);
 	private IntWritable movieId = new IntWritable();
 	
@@ -39,46 +39,53 @@ public class Map extends Mapper<Object, Text, Text, NullWritable> {
 		else{
 			try{
 				double d = Double.parseDouble(str[1]);
-				top20.put(value,d);
-				//System.out.println("Curent Values : " + d + " - " + value.toString() + "\n Size : " + top20.size());
+				top20.put(value.toString(),d);
+				System.out.println("Curent Values : " + d + " - " + value.toString() + "\n Size : " + top20.size());
 				if(top20.size()>10)
 				{
-					Set<Entry<Text, Double>> set = top20.entrySet();
-					List<Entry<Text, Double>> list = new ArrayList<Entry<Text, Double>>(
+					Set<Entry<String, Double>> set = top20.entrySet();
+					List<Entry<String, Double>> list = new ArrayList<Entry<String, Double>>(
 							set);
-					Collections.sort(list, new Comparator<Entry<Text, Double>>() {
-						public int compare(Entry<Text, Double> o1,
-										   Entry<Text, Double> o2) {
+					Collections.sort(list, new Comparator<Entry<String, Double>>() {
+						public int compare(Entry<String, Double> o1,
+										   Entry<String, Double> o2) {
 							return o2.getValue().compareTo(o1.getValue());
 						}
 					});
 					/*System.out.println("Removing Line : " + top20.firstKey());*/
-					list.remove(10);
+					/*System.out.println(list.get(10));
+					System.out.println(list.remove(10));*/
 
-					top20 = new HashMap<Text, Double>();
-					for (Entry<Text, Double> j : list)
-						top20.put(j.getKey(),j.getValue());
+					top20.clear();
+					int i = 0;
+					for (Entry<String, Double> j : list)
+						if(i++<10) {
+						System.out.println("After Removing : " + j.getKey() + j.getValue());
+							top20.put(j.getKey(), j.getValue());
+						}
 				}
-				/*System.out.println("Remaning records : " + top20);*/
 			}catch (NumberFormatException e) {
 				System.out.println("Found Improper movieId => \"" + str[1] + "\"");
 			}
+			System.out.println("Remaning records : " + top20);
 		}
 	}
 	@Override
 	protected void cleanup(Context context)
 			throws IOException, InterruptedException {
 
-		Set<Entry<Text, Double>> set = top20.entrySet();
-		List<Entry<Text, Double>> list = new ArrayList<Entry<Text, Double>>(
+		Set<Entry<String, Double>> set = top20.entrySet();
+		List<Entry<String, Double>> list = new ArrayList<Entry<String, Double>>(
 				set);
-		Collections.sort(list, new Comparator<Entry<Text, Double>>() {
-			public int compare(Entry<Text, Double> o1,
-							   Entry<Text, Double> o2) {
+		Collections.sort(list, new Comparator<Entry<String, Double>>() {
+			public int compare(Entry<String, Double> o1,
+							   Entry<String, Double> o2) {
 				return o2.getValue().compareTo(o1.getValue());
 			}
 		});
-		for (Entry<Text, Double> j : list)
-			context.write(j.getKey(),NullWritable.get());
+		for (Entry<String, Double> j : list) {
+			System.out.println("Final Output : " + j.getKey() + " -- " + NullWritable.get());
+			context.write(new Text(j.getKey()), NullWritable.get());
+		}
 	}
 }
